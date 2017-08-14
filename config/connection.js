b1b5fd41675c381be7b095d5cb628f72c4e40c60
Jonathan Grim <jonjongrim@gmail.com>
2017-08-14 17:45:12 -0400

    Update with Heroku DB

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,3 @@
 const mysql = require('mysql2');
 
-module.exports = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'burger_db'
-});
+module.exports = mysql.createConnection(process.env.JAWSDB_URL);
